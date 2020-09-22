236ea0553b2cd6bce4bfe5ae0517aefdc617945d
Jeremy Spradlin <spradlinjk@gmail.com>
2020-09-22 19:13:09 -0400

    Doing some black magic

--- app/api/app_db.py
+++ app/api/app_db.py
@@ -3,8 +3,9 @@ import sqlite3
 
 
 def create_db():
-    df = pd.read_csv('../../data/cannabis_final.csv')
-    conn = sqlite3.connect('../../data/cannabis.sqlite3')
+    df = pd.read_csv('data/cannabis_final.csv')
+    df = df.drop('Unnamed: 0', axis=1)
+    conn = sqlite3.connect('data/cannabis.sqlite3')
     curs = conn.cursor()
     curs.execute("DROP TABLE IF EXISTS Cannabis")
     df.to_sql("Cannabis", con=conn)
--- app/api/predict.py
+++ app/api/predict.py
@@ -7,6 +7,8 @@ from sklearn.neighbors import NearestNeighbors
 from spacy.tokenizer import Tokenizer
 from spacy.lang.en import English
 from .app_db import create_db
+from fastapi.responses import JSONResponse
+import sqlite3
 
 
 log = logging.getLogger(__name__)
@@ -14,7 +16,7 @@ router = APIRouter()  # this establishes what router we are using (i.e FLASK)
 df = pd.read_csv("data/cannabis_final.csv")
 
 
-def searchfunc(user_input, num_results=10):  # this is the function steve and jeremy will give me, used in ML training
+def searchfunc(user_input, num_results=1):  # this is the function steve and jeremy will give me, used in ML training
     """Flexible function that searches for cannabis strains.
     ### Request Body
     - user_input str
@@ -24,8 +26,8 @@ def searchfunc(user_input, num_results=10):  # this is the function steve and je
     """
     user_input = [user_input]
     nlp = English()
-    tokenizer = Tokenizer(nlp.vocab)
-    tf = TfidfVectorizer(tokenizer=tokenizer, stop_words='english')
+    #tokenizer = Tokenizer(nlp.vocab)
+    tf = TfidfVectorizer(stop_words='english')
     dtm = tf.fit_transform(df['ailment_tokens'])
     dtm = pd.DataFrame(dtm.todense(), columns=tf.get_feature_names())
     nr = num_results
@@ -33,27 +35,29 @@ def searchfunc(user_input, num_results=10):  # this is the function steve and je
     nn.fit(dtm)
     dtf = tf.transform(user_input)
     _, output = nn.kneighbors(dtf.todense())
-    recommendations = []
-    for n in output:
-        for row in n:
-            recommendations.append(row)
-    result = []
-    for i in recommendations:
-        data = (df.loc[i, :])
-        result.append(data)
-    return {'strain_recommendations': result}
+    return output
+    # recommendations = []
+    # for n in output:
+    #     for row in n:
+    #         recommendations.append(row)
+    # result = []
+    # for i in recommendations:
+    #     data = (df.loc[i, :])
+    #     result.append(data)
+    # #return {'strain_recommendations': result}
+    # return result
 
 
 class Item(BaseModel):
     """Use this data model to parse the request body JSON."""
 
-    symptoms: str = Field(..., example='insomia')  # this takes any symptom as a string
+    symptoms: str #= Field(..., example='insomia')  # this takes any symptom as a string
     # can also put a field for 'effects', 'flavors', 'strain', (if they do not concatnate the strings for us)
 
-    def to_df(self):  # this will take our symptoms and takes it as a dictionary and makes it a dataframe for the model
-                      # to use
-        """Convert pydantic object to pandas dataframe with 1 row."""
-        return pd.DataFrame([dict(self)])
+    # def to_df(self):  # this will take our symptoms and takes it as a dictionary and makes it a dataframe for the model
+    #                   # to use
+    #     """Convert pydantic object to pandas dataframe with 1 row."""
+    #     return pd.DataFrame([dict(self)])
 
     # @validator('x1')
     # def x1_must_be_positive(cls, value):
@@ -68,9 +72,21 @@ async def predict(item: Item):
       # Make random baseline predictions for classification problem 🔮
       """
 
-    X_new = item.to_df()
-    log.info(X_new)
-    return {'recommendations': searchfunc(item.symptoms, num_results=5)}
+    pred = searchfunc(item.symptoms)
+
+    conn = sqlite3.connect('data/cannabis.sqlite3')
+    curs = conn.cursor()
+    print(pred)
+    curs.execute(f"SELECT * FROM Cannabis WHERE strain_id == {pred[0][0]}")
+
+    strain = curs.fetchall()
+    print(strain)
+
+
+
+    #return 'Prediction!'
+    return JSONResponse(strain)
+    #return {'recommendations': searchfunc(item.symptoms, num_results=5)}
 
 
 @router.get('/createDB')
